c3c81d620bc0bc4bc4c1ed85419e570bc3c8e648
Tristan Putra <Ngurah.TristanPutra@outlook.jp>
2023-07-03 13:32:42 +0800

    a

--- cypress/e2e/portofolio2.cy.ts
+++ cypress/e2e/portofolio2.cy.ts
@@ -3,10 +3,10 @@ describe('template spec', () => {
     cy.visit('http://localhost:3000')
   })
   it('Navbar test',()=>{
-    // cy.get('.active').click()
-    // cy.get('.flex-1 > .items-center > :nth-child(2)').click()
-    // cy.get('.flex-1 > .items-center > :nth-child(3)').click()
-    // cy.get('.flex-1 > .items-center > :nth-child(4)').click()
+    cy.get('.active').click()
+    cy.get('.flex-1 > .items-center > :nth-child(2)').click()
+    cy.get('.flex-1 > .items-center > :nth-child(3)').click()
+    cy.get('.flex-1 > .items-center > :nth-child(4)').click()
   })
   it('Project Test',()=>{
     // cy.get('#project1').click()
@@ -18,20 +18,23 @@ describe('template spec', () => {
     // cy.get('#project3').click()
     // cy.go('back')
     cy.get(':nth-child(1) > .h-fit > .p-4 > .flex > a > .visit-button').click()
+    cy.get(':nth-child(2) > .h-fit > .p-4 > .flex > a > .visit-button').click()
+    cy.get(':nth-child(3) > .h-fit > .p-4 > .flex > a > .visit-button').click()
+
   it('button test',()=>{
     // cy.get('#home').screenshot()
     // cy.get('#home').click()
   })
   })
   it('Form Test',()=>{
-  // cy.fillOutForm({
-  //   // name: 'Cypress',
-  //   // email : 'cypress@email.com',
-  //   // message : 'Ok'
-  // });
+  cy.fillOutForm({
+    name: 'Cypress',
+    email : 'cypress@email.com',
+    message : 'Ok'
+  });
   })
   it('Responsive Test',()=>{
-    cy.viewport('iphone-xr').screenshot()
+    cy.viewport('iphone-xr')
   })
 })
 export{}
